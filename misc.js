b297eb1d59a51a4bbc114e9f721e2f57eff9f77a
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-11-06 19:21:02 -0500

    added socks to misc.js how many pairs of socks in array

--- misc.js
+++ misc.js
@@ -24,3 +24,25 @@ const convert = (num) => {
     }
     return (hr + ':' + min)
 }
+
+/* 2. Matching Socks - Hackerrank problem. Given an array of numbers, each number 
+representing a color, find how many matching pairs in the array */
+
+const socks = (arr) => {
+    let count = {};
+    for (let i = 0; i<arr.length; i++) {
+        let sock = arr[i];
+        if (count[sock]) {
+            count[sock]++;
+        } else {
+            count[sock]=1;
+        }
+    }
+    let pairs = 0;
+    for (let num in count) {
+        if(count[num] > 1){
+             pairs += Math.floor(count[num]/2);
+        }
+    }
+    return pairs
+}
