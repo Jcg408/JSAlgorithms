4b49e03c6c8d0b3cec5a0a65b846e81dbcb31c5b
Jcg408 <jcg321@comcast.net>
2020-01-11 15:22:00 -0500

    add Time Conversion to hrank.js

--- hrank.js
+++ hrank.js
@@ -110,3 +110,21 @@ const socks = arr => {
         }
         return holes;
     }
+ /* Time Conversion - Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM 
+on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+Function Description:
+Complete the timeConversion function in the editor below. It should return a new string representing the 
+input time in 24 hour format. The function has the following parameter(s):   a string representing time in  hour format. */
+
+const timeConvert = str => {
+      let newStr = str.split(/\D/);
+   
+    if (str.startsWith('12') && str.endsWith('AM')) {
+        newStr[0] = '00';
+    } else if (!str.startsWith('12') && str.endsWith('PM')) {
+        newStr[0] = parseInt(newStr[0]) + 12;
+        newStr[0].toString();
+    }
+    return newStr.join(':').slice(0, 8);
+};
